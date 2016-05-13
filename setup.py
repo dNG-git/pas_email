23bc1aefb62a2098a8830c2ec06a6f300d25a678
NotTheEvilOne <git-NTEO@vplace.de>
2016-05-13 12:01:59 +0200

    Trivial: Added "platforms" list to "setup.py" Trivial: Added "et al." to the authors attribute in "setup.py"

--- setup.py
+++ setup.py
@@ -52,10 +52,12 @@ with TemporaryDirectory(dir = ".") as build_directory:
 	      version = get_version(),
 	      description = "Python Application Services",
 	      long_description = """"pas_email" provides a simple to use SMTP client based on "rfc_email.py".""",
-	      author = "direct Netware Group",
+	      author = "direct Netware Group et al.",
 	      author_email = "web@direct-netware.de",
 	      license = "MPL2",
 	      url = "https://www.direct-netware.de/redirect?pas;email",
+
+	      platforms = [ "any" ],
 
 	      package_dir = { "": _build_path },
 	      packages = [ "dNG" ],
